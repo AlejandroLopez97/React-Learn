f3a96e63e82341246525e37a2d3c0b0df53f469f
AlejoOliveros <alejooliveros97@outlook.com>
2025-01-24 16:28:35 -0500

    Se realiza uso de state en app.js que es componente padre, para los componentes TodoCounter y TodoSearch

--- src/App.js
+++ src/App.js
@@ -14,17 +14,36 @@ const defaultTodos = [
 ];
 
 function App() {
+
+  const [todos, setTodos] = React.useState(defaultTodos); //Estado que me permite contar la lista y enviarse al todoCounter como props
+
+  const [searchValue, setSearchValue] = React.useState(''); //Estado que me permite manejar la busqueda del input y enviarse al todoSearch y al todoList para filtrar
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length; //como el estado es un filtro, busco los que cuentan con la propiedad completed como true, considerado un estado derivado de todos
+  const totalTodos = todos.length; //los cuento todos
+
+  //console.log('los usuarios buscan todos de ' + searchValue);
+
   return (
       // <div className="App"> Tambien se puede hacer por medio de react fragment, que se encargar de hacer en bloque lo que retornará la función
       // al poner react fragment se daña el estilo que se encuentra aplicado sobre la clase app
       <React.Fragment>
-        <TodoCounter completed={16} totalTodos={25}/>
-        <TodoSearch/>
+        <TodoCounter 
+          completed={completedTodos} 
+          totalTodos={totalTodos}
+        />
+
+        <TodoSearch
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+        />
 
         <TodoList>
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
           {defaultTodos.map(todo => (
             <TodoItem 
-              key={todo.text} 
+              key = {todo.text} 
               text = {todo.text}
               completed = {todo.completed}
             />
--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,7 +1,8 @@
-
+import React from 'react';
 import './TodoCounter.css'
 
 function TodoCounter({ totalTodos, completed }){ //Recibimos la propiedad en la funcion de manera destructurada, tambien se puede recibir completa
+
     return(
         <h1 className='TodoCounter'>
             Has completado <span>{completed}</span> de <span>{totalTodos}</span> TODOS
