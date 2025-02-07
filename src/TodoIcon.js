cbb57d02a59706036ec5d3e847b648d43b1c99af
AlejoOliveros <alejooliveros97@outlook.com>
2025-02-07 13:43:39 -0500

    Se separan los estilos de los iconos en un archivo propio

--- src/TodoIcon.js
+++ src/TodoIcon.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { ReactComponent as CheckSVG } from './check.svg';
 import { ReactComponent as DeleteSVG } from './delete.svg';
+import './TodoIcon.css';
 
 //Creo un objeto con los tipos de iconos que se desean mostrar 
 const iconTypes = {
-    "check": <CheckSVG/>,
-    "delete": <DeleteSVG/>
+    "check": <CheckSVG className="Icon-svg" />,
+    "delete": <DeleteSVG className="Icon-svg" />
 }
 
 function TodoIcon({ type }){
     return(
         <span
-            className = {`Icon Icon-svg Icon-${type}`}
+            className = {`Icon-container Icon-container-${type}`}
         >
             {iconTypes[type]}
         </span>
